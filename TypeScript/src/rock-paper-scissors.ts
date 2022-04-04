e80690679eb2d2c0a452c09fc0602c1ad01b02a0
André Coelho <andre.coelho@capitalise.com>
2022-04-04 11:49:20 +0100

    solution team 1

--- TypeScript/src/rock-paper-scissors.ts
+++ TypeScript/src/rock-paper-scissors.ts
@@ -1,7 +1,55 @@
-export class RockPaperScissors {
-  constructor(private readonly input: string) {}
+class Player {
+  constructor(private move: string) {
+    this.move = move;
+  }
 
-  play() {
-    
+  getMove() {
+    return this.move;
   }
 }
+
+class RulesEngine {
+
+  private rules = {
+    scissors: "paper",
+    paper: "rock",
+    rock: "scissors",
+  };
+
+  getWinner(player1: Player, player2: Player): string {
+    const player1Move = player1.getMove();
+    const player2Move = player2.getMove();
+
+    if (player1Move === player2Move) {
+      return "Draw";
+    }
+
+    if (this.rules[player1Move] === player2Move) {
+      return `Player 1 wins: ${player1Move} beats ${player2Move}`;
+    }
+    return `Player 2 wins: ${player2Move} beats ${player1Move}`;
+  }
+}
+
+export class RockPaperScissors {
+  private player1: Player;
+  private player2: Player;
+
+  constructor(private readonly input: string) {
+    this.player1 = new Player(this.input.split(" ")[0]);
+    this.player2 = new Player(this.input.split(" ")[1]);
+  }
+
+  play(): String {
+    const engine = new RulesEngine();
+    return engine.getWinner(this.player1, this.player2);
+  }
+}
+
+// André Carvalho
+// Pedro Alves
+// André Coelho
+// Brandão
+// Anna
+// José
+// Solomiya
--- TypeScript/test/rock-paper-scissors.spec.ts
+++ TypeScript/test/rock-paper-scissors.spec.ts
@@ -1,8 +1,44 @@
-import { RockPaperScissors } from '@/rock-paper-scissors';
+import { RockPaperScissors } from "@/rock-paper-scissors";
 
-describe('Rock, Paper, Scissors Game Unit Tests', () => {
-  it('rock beats scissors', () => {
-    const game = new RockPaperScissors("rock paper");
-    expect(game.play).toEqual("Player 2 wins: paper beats rock");
+describe("Rock, Paper, Scissors Game Unit Tests", () => {
+  describe("Player 1 wins", () => {
+    it("Rock beats scissors", () => {
+      const game = new RockPaperScissors("rock scissors");
+      expect(game.play()).toEqual("Player 1 wins: rock beats scissors");
+    });
+
+    it("Paper beats Rock", () => {
+      const game = new RockPaperScissors("paper rock");
+      expect(game.play()).toEqual("Player 1 wins: paper beats rock");
+    });
+
+    it("Scissors beats Paper", () => {
+      const game = new RockPaperScissors("scissors paper");
+      expect(game.play()).toEqual("Player 1 wins: scissors beats paper");
+    });
+  });
+
+  describe("Player 2 wins", () => {
+    it("Rock beats scissors", () => {
+      const game = new RockPaperScissors("scissors rock");
+      expect(game.play()).toEqual("Player 2 wins: rock beats scissors");
+    });
+
+    it("Paper beats Rock", () => {
+      const game = new RockPaperScissors("rock paper");
+      expect(game.play()).toEqual("Player 2 wins: paper beats rock");
+    });
+
+    it("Scissors beats Paper", () => {
+      const game = new RockPaperScissors("paper scissors");
+      expect(game.play()).toEqual("Player 2 wins: scissors beats paper");
+    });
+  });
+
+  describe("Draw", () => {
+    it("Should return Draw", () => {
+      const game = new RockPaperScissors("paper paper");
+      expect(game.play()).toEqual("Draw");
+    });
   });
 });

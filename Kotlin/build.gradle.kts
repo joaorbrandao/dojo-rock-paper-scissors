36dc54acdecca156edccf42a16c5b987430409a0
joaorbrandao <joaorbrandao.dev@outlook.pt>
2024-11-12 13:38:23 +0000

    Upgrade Kotlin version

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,7 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "2.0.21"
     application
 }
 
@@ -20,8 +21,11 @@ tasks.test {
     useJUnitPlatform()
 }
 
+
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
 }
 
 application {
